f16a8bfc19862b80297d67d61ca77512cfbd551c
paintspot87 <ajn19017@student.mdu.se>
2024-03-12 10:23:10 +0100

    fixed python sendscript generator to create new files and move the into the main folder

--- inet/examples/hitachi_test/scripts/tcpSessionApp_sendscript_generator.py
+++ inet/examples/hitachi_test/scripts/tcpSessionApp_sendscript_generator.py
@@ -1,5 +1,39 @@
 import numpy as np
-import os
+import os, glob
+
+noOfPackets = 0
+simulationStart = 0
+simulationEnd = 0
+bytesPerPacket = 0
+noOfFiles = 0
+filename = ""
+
+def configuration_mms_traffic():
+    global noOfPackets, simulationStart, simulationEnd, bytesPerPacket, noOfFiles, filename
+    noOfPackets = 8333
+    simulationStart = 0
+    simulationEnd = 10
+    bytesPerPacket = 84
+    noOfFiles = 3
+    filename = "sendscript.txt"
+
+def configuration_it_traffic():
+    global noOfPackets, simulationStart, simulationEnd, bytesPerPacket, noOfFiles, filename
+    noOfPackets = 833
+    simulationStart = 0
+    simulationEnd = 10
+    bytesPerPacket = 1434
+    noOfFiles = 4
+    filename = "sendscriptIT.txt"
+
+def configuration_log_traffic():
+    global noOfPackets, simulationStart, simulationEnd, bytesPerPacket, noOfFiles, filename
+    noOfPackets = 50
+    simulationStart = 0
+    simulationEnd = 10
+    bytesPerPacket = 84
+    noOfFiles = 12
+    filename = "sendscriptLOG.txt"
 
 def generate_sorted_numbers(noOfPackets, simulationStart, simulationEnd, bytesPerPacket):
     random_numbers = np.random.uniform(low=simulationStart, high=simulationEnd, size=noOfPackets)
@@ -23,14 +57,40 @@ def write_to_file(filename, content):
         file.write(content)
     print(f"Output written to {unique_filename}")
     
-noOfPackets = 833
-simulationStart = 0
-simulationEnd = 10
-bytesPerPacket = 1434
-noOfFiles = 4
-filename = "sendscriptIT.txt"
+def generate_scripts(script):
+    if script == "mms":
+        configuration_mms_traffic()
+        for i in range(noOfFiles):
+            output = generate_sorted_numbers(noOfPackets, simulationStart, simulationEnd, bytesPerPacket)
+            write_to_file(filename, output)
+    elif script == "log":
+        configuration_log_traffic()
+        for i in range(noOfFiles):
+            output = generate_sorted_numbers(noOfPackets, simulationStart, simulationEnd, bytesPerPacket)
+            write_to_file(filename, output)
+    elif script == "it":
+        configuration_it_traffic()
+        for i in range(noOfFiles):
+            output = generate_sorted_numbers(noOfPackets, simulationStart, simulationEnd, bytesPerPacket)
+            write_to_file(filename, output)
+            
+def remove_text_files():
+    files = glob.glob('./*.txt')
+    for f in files:
+        os.remove(f)
+        
+def move_text_files():
+    files = glob.glob('./*.txt')
+    for f in files:
+        os.replace(f, '../'+f) 
+        
+remove_text_files()
+generate_scripts("mms")
+generate_scripts("log")
+generate_scripts("it")
+move_text_files()
 
-for i in range(noOfFiles):
-    output = generate_sorted_numbers(noOfPackets, simulationStart, simulationEnd, bytesPerPacket)
-    write_to_file(filename, output)
+
+
+
     
